3e94e41e3e17d8e04587d9dd99673edf09be393d
iiiGYGYiii <ococoletzic@gmail.com>
2021-11-12 00:07:26 -0600

    Create express server and ping route

---
+++ server/src/config/index.ts
@@ -0,0 +1,1 @@
+export const PORT: string | 3001 = process.env.PORT || 3001;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { PORT } from "./config";
+import { pingRouter } from "./routers";
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("tiny"));
+app.use("/ping", pingRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
